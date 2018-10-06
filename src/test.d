b56a5a22a08c6530727a16653b14ea4c22ff9eb1
DYGV <DYGV@users.noreply.github.com>
2018-10-07 00:20:25 +0900

    add function of file creation for data

--- src/test.d
+++ src/test.d
@@ -7,6 +7,9 @@ int main(){
     auto parsed_data = parse("../png_img/lena.png");
     if(parsed_data.length != 0)
         parsed_data.each!(n  => actual_data ~= n.chunks(length_per_pixel).array);
+    auto rgb_file = File("rgb_lena.txt","w");
+    rgb_file.writeln(actual_data);
+
     return 0;
 }
 
