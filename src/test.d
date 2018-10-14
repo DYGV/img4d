ba84f94649dd6e3e622c23abd43a27bc617d71c0
DYGV <DYGV@users.noreply.github.com>
2018-10-14 22:39:49 +0900

    change file creation path

--- src/test.d
+++ src/test.d
@@ -7,14 +7,14 @@ int main(){
     auto parsed_data = parse("../png_img/lena.png");
     if(parsed_data.length != 0)
         parsed_data.each!(n  => actual_data ~= n.chunks(length_per_pixel).array);
-    auto rgb_file = File("rgb_lena.txt","w");
+    auto rgb_file = File("../png_img/rgb_lena.txt","w");
     rgb_file.writeln(actual_data);
 
   /*
      covert to grayscale
    */
     auto gray = to_grayscale(actual_data);
-    auto gray_file = File("gray_lena.txt","w");
+    auto gray_file = File("../png_img/gray_lena.txt","w");
     gray_file.writeln(gray);
     return 0;
 }
